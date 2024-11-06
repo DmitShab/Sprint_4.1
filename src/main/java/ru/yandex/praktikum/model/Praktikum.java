acc333a5dc7ee59dcae603ebdde287e95a26e54e
Dmitry Shabalin <67576062+DmitShab@users.noreply.github.com>
2024-11-06 20:23:11 +0300

    Delete src/main/java/ru/yandex/praktikum directory

--- src/main/java/ru/yandex/praktikum/model/Praktikum.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.praktikum.model;
-
-public class Praktikum {
-    public static void main(String[] args) {
-
-    }
-}
--- src/main/java/ru/yandex/praktikum/model/constants/Discount.java
+++
@@ -1,6 +0,0 @@
-package ru.yandex.praktikum.model.constants;
-
-public class Discount {
-    public static final double DISCOUNT1 = 60.0;
-    public static final double NO_DISCOUNT = 0.0;
-}
